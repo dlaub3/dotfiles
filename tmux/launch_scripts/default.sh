d5fbb86d9c6088a01d79e2f033f0bfbef7e2e44b
Daniel Laubacher <daniel@simspace.com>
2020-01-03 21:26:41 -0500

    update tmux

--- tmux/launch_scripts/default.sh
+++ tmux/launch_scripts/default.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
 # Set some configuration options
 SESSION=$USER
 
 # Create a new, detached session
 tmux new-session -d -s $SESSION
+tmux send-keys 'cd ~/Documents/dev' C-m
 
 # Create a second window
 tmux new-window -t $SESSION:1 -n 'default'
-
 # Select and configure the first window
 tmux select-window -t $SESSION:1
-tmux split-window -v 
-tmux send-keys 'cd ~/Documents' C-m
+tmux split-window -v
+tmux select-pane -t $SESSION:1
+tmux split-pane -h
 
 # Attach to the session
 tmux attach-session -t $SESSION
