b49c4e80e23c99013abfb63f4326eaec6e359aa6
Daniel Laubacher <dlaubacher3@gmail.com>
2020-02-29 15:29:53 -0500

    update install for gitconfig

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ ln -nfs ~/.dotfiles/config/nvim ~/.config/nvim
 ln -nfs ~/.dotfiles/tmux  ~/.tmux
 ln -nfs ~/.dotfiles/tmux.conf  ~/.tmux.conf
 ln -nfs ~/.dotfiles/gitignore_global  ~/.gitignore_global
-# ln -nfs ./gitconfig  ~/.gitconfig # This might overwrite important info for [user]
+ln -nfs ~/.dotfiles/gitconfig  ~/.gitconfig # This might overwrite important info for [user]
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
