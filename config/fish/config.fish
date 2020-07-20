a5b6a9a52df9b393f59e65d54f720a63852017aa
Daniel Laubacher <daniel@simspace.com>
2020-07-20 12:56:26 -0400

    update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,5 @@
 # alias
+alias ls="exa --long --header"
 alias ll="ls -alh"
 alias gcap="git commit -am \"make prettier 💋\""
 
@@ -8,20 +9,54 @@ alias tmux-default="~/.tmux/launch_scripts/default.sh"
 # JS Helpers
 alias git-changed="git diff --name-only --relative && git diff --staged --name-only --relative && git ls-files -o --exclude-standard"
 
-function jsBeautifyAge --description "beautify <interval>" 
-  npx prettier --write (git diff --name-only --relative "@{$argv[1]}")
+alias jsonPretty="pbpaste | jq | pbcopy && pbpaste | jq"
+
+function jsImport --description "copy the import path to the clipboard"
+   rg " $argv[1]" -l | rg "$argv[1]" | pbcopy
 end
 
-function jsLintAge --description "lintAge <interval>"
-  npx tslint (git diff --name-only --relative "@{$argv[1]}" | grep '\.[t|j]sx\?$' ) --fix
+function jsBeautifyAge --description "beautify <interval>" 
+  npx prettier@1.19.1 --write (git diff --name-only --relative "@{$argv[1]}")
 end
 
 function jsBeautify --description "beautify"
-  npx prettier --write (git-changed)
+  npx prettier@1.19.1 --write (git-changed)
 end
 
-function jsLint --description "lint"
-  npx tslint (git-changed | grep '\.[t|j]sx\?$') --fix -c ./.tslintrc.ignore.json
+function styleLint --description "lint"
+  set files_list (git-changed | grep '\.[t|j]sx\?$')
+  if test "$files_list"
+    npx stylelint@12.0.0 $files_list --config ./.stylelint.config.js
+  else
+    echo "no files"
+  end
+end
+
+function tsLint --description "lint"
+  set files_list (git-changed | grep '\.[t|j]sx\?$')
+  if test "$files_list"
+    npx tslint $files_list --fix -c ./.tslintrc.ignore.json
+  else
+    echo "no files"
+  end
+end
+
+function tsLintNoSort --description "lint"
+  set files_list (git-changed | grep '\.[t|j]sx\?$')
+  if test "$files_list"
+    npx tslint $files_list --fix
+  else
+    echo "no files"
+  end
+end
+
+function tsLintAge --description "lintAge <interval>"
+  set files_list (git diff --name-only --relative "@{$argv[1]}" | grep '\.[t|j]sx\?$')
+  if test "$files_list"
+    npx tslint $files_list --fix
+  else
+    echo 'no files'
+  end
 end
 
 function showFiles --description "search files and list filenames"
@@ -31,3 +66,5 @@ end
 function showContents --description "search files and list contents"
   egrep -C 3 -rl $argv[1] . | xargs egrep -C2 --color $argv[1]
 end
+
+starship init fish | source
