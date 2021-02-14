5ca7b7ea3d895953f28d2ad9c9fdd60cb7b8653f
Daniel Laubacher <dlaubacher3@gmail.com>
2021-02-14 08:50:50 -0500

    add nuke

---
+++ scripts/nuke.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# 
+# https://revelry.co/resources/development/terminal-workflow-fzf/
+#
+# 
+nuke() {
+  local pid
+  pid=$(ps -ef | grep -v ^root | sed 1d | fzf -m | awk '{print $2}')
+
+  if [ "x$pid" != "x" ]
+  then
+    echo $pid | xargs kill -${1:-9}
+  fi
+}
+
+nuke
