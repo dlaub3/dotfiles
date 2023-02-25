897d77fc6380c23b444f9f4e7980fe188594364c
Daniel Laubacher <dlaubacher3@gmail.com>
2023-02-25 10:24:41 -0500

    add rofi

--- install.sh
+++ install.sh
@@ -11,4 +11,6 @@ ln -nfs ~/.dotfiles/ripgreprc  ~/.ripgreprc
 mkdir ~/.vimsessions
 ln -nfs ~/.dotfiles/gitconfig  ~/.gitconfig # This might overwrite important info for [user]
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+touch ~/.local-env.fish
+touch ~/.local-env.sh
 #npm install -g instant-markdown-d
