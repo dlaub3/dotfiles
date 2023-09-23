64143bf98c55cd0696ac285ada946d56f610fd2f
Daniel Laubacher <dlaubacher3@gmail.com>
2023-09-23 12:05:22 -0700

    font size

--- scripts/scale-monitors.sh
+++ scripts/scale-monitors.sh
@@ -2,5 +2,5 @@
 
 sed -i 's/--dpi 196/--dpi 100/' $HOME/.xinitrc
 sed -i 's/--dpi 196/--dpi 100/' $HOME/.config/i3/config
-sed -i 's/monospace 18/monospace 12/' $HOME/.config/i3/config
+sed -i 's/monospace 14/monospace 12/' $HOME/.config/i3/config
 sed -i 's/Mono 24/Mono 12/' $HOME/.config/rofi/config.rasi
