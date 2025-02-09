bfd813c9eb016c74c74c42ac2523eb17e036255a
Daniel Laubacher <dlaubacher3@gmail.com>
2025-02-08 20:29:08 -0500

    i3

--- config/i3/scripts/toggle_btop.sh
+++ config/i3/scripts/toggle_btop.sh
@@ -4,17 +4,18 @@ source $HOME/.config/i3/scripts/floating_resize.sh
 
 WINDOW_ID=$(i3-msg -t get_tree | jq -r '.. | objects | select(.window) | select(.name=="btop").window')
 IS_FOCUSED=$(i3-msg -t get_tree | jq -r '.. | objects | select(.window) | select(.name=="btop").focused')
-ACTIVE_WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).name')
 
 if [ -n "$WINDOW_ID" ]; then
     if  [[ "${IS_FOCUSED,,}" != "true" ]]; then
+        i3-msg "[instance=\"btop\"] floating enable;"
         i3-msg "[instance=\"btop\"] move container to workspace current;"
         i3-msg "[instance=\"btop\"] resize set $WIDTH $HEIGHT"
         i3-msg "[instance=\"btop\"] move position center"
         i3-msg "[instance=\"btop\"] focus"
     else
-        i3-msg "[instance=\"btop\"] resize set 10 10"
         i3-msg "[instance=\"btop\"] move to workspace 🙈"
+        i3-msg "[instance=\"btop\"] floating disable;"
+        workspace 🙈 layout splitv
     fi
 else
     # If the window doesn't exist, launch it
--- config/i3/scripts/toggle_chatgpt.sh
+++ config/i3/scripts/toggle_chatgpt.sh
@@ -4,17 +4,18 @@ source $HOME/.config/i3/scripts/floating_resize.sh
 
 WINDOW_ID=$(i3-msg -t get_tree | jq -r '.. | objects | select(.window) | select(.name=="ChatGPT").window')
 IS_FOCUSED=$(i3-msg -t get_tree | jq -r '.. | objects | select(.window) | select(.name=="ChatGPT").focused')
-ACTIVE_WORKSPACE=$(i3-msg -t get_workspaces | jq -r '.[] | select(.focused).name')
 
 if [ -n "$WINDOW_ID" ]; then
     if  [[ "${IS_FOCUSED,,}" != "true" ]]; then
+        i3-msg "[instance=\"chatgpt\"] floating enable;"
         i3-msg "[instance=\"chatgpt\"] move container to workspace current;"
         i3-msg "[instance=\"chatgpt\"] resize set $WIDTH $HEIGHT"
         i3-msg "[instance=\"chatgpt\"] move position center"
         i3-msg "[instance=\"chatgpt\"] focus"
     else
-        i3-msg "[instance=\"chatgpt\"] resize set 10 10"
         i3-msg "[instance=\"chatgpt\"] move to workspace 🙈"
+        i3-msg "[instance=\"chatgpt\"] floating disable;"
+        workspace 🙈 layout splitv
     fi
 else
     # If the window doesn't exist, launch it
