114f7a4dc6fd81a5ec89cbaa383b8467fec8336d
Daniel Laubacher <dlaubacher3@gmail.com>
2025-02-10 08:53:35 -0500

    fix scratchpad resize

--- config/i3/scripts/scratchpad_resize.sh
+++ config/i3/scripts/scratchpad_resize.sh
@@ -4,7 +4,7 @@ i3-msg scratchpad show
 source $HOME/.config/i3/scripts/floating_resize.sh
 WINDOW_ID=$(i3-msg -t get_tree | jq '.. | objects | select(.focused == true) | select(.floating == "user_on").window')
 
-if [[ -z "${WINDOW_ID}" ]]; then
+if [[ -n "${WINDOW_ID}" ]]; then
   i3-msg [id="$WINDOW_ID"] resize set "${WIDTH}px" "${HEIGHT}px"
   i3-msg [id="$WINDOW_ID"] move position center
 fi
